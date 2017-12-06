1c62aeda45581aaf62c993b825ba8559e2c49a2b
Maja Persson <persson.maja@gmail.com>
2017-12-06 12:19:09 +0100

    Created gameLogic function and moved to functions.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 
+// TO DO
+// - Change numbers to images
+// - Fix start screen (with innerHTML-function)
+
 // OPTIONAL
 // - Add difficulty (8, 10 or 12 pairs)
 
-// Fråga Vincent:
-// Varför ändras stack när inget värde returneras?
 
 'use strict';
 
@@ -15,50 +17,17 @@ for(let i=0; i<pairs; i++){   // Adds duplicates for each card in stack
   stack.push(i, i);
 };
 
+// CREATE BOARD
 createBoard(stack);
+gameLogic();
 
-const cards = document.querySelectorAll('.card'); // Creates array of cardDivs
-
-let clickArray = [];
-let matchArray = [];
-let counter = 0;
-
-cards.forEach((card) => {     // Adds EventListener for each cardDiv
-  card.addEventListener('click', (event) => {
-    event.target.classList.add('clicked'); // Flips card
-    clickArray.push(event.target.dataset.number); // Adds data number to array
-    matchArray.push(card);
-
-    if (clickArray.length === 2) {
-      if (clickArray[0] === clickArray[1]){ // If cards data number match
-        matchArray[0].classList.add('matched');
-        matchArray[0].classList.remove('clicked');
-        matchArray[1].classList.add('matched');
-        matchArray[1].classList.remove('clicked');
-        clickArray = [];
-        matchArray = [];
-        counter++;
-      } else {
-        setTimeout(()=> {
-        matchArray[0].classList.remove('clicked');
-        matchArray[1].classList.remove('clicked');
-        clickArray = [];
-        matchArray = [];
-        }, 800);
-      };
-    };
-
-    if (counter === pairs) {
-      document.querySelector('.complete').classList.add('visible');
-    };
-
-  });
-});
-
+// RESET BUTTON
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     resetBoard();
+    createBoard(stack);
+    gameLogic();
   });
 });
