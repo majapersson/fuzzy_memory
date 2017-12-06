bb1d2cebc94dacbcdf806525f59dad50690e205d
Maja Persson <persson.maja@gmail.com>
2017-12-06 12:20:07 +0100

    New shuffle function, cleaned up code and added gameLogic()

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,7 @@
 /*
 * Shuffles stack and creates card div for each item in stack array
+*
+* @param {array} array
 */
 const createBoard = (array) => {
   const shuffledStack = shuffle(array); // Shuffles stack
@@ -10,42 +12,76 @@ const createBoard = (array) => {
   });
 };
 
+// GAME LOGIC
+const gameLogic = () => {
+  const cards = document.querySelectorAll('.card'); // Creates array of cardDivs
+
+  let clickArray = [];
+  let matchArray = [];
+  let counter = 0;
+
+  console.log('Hello gamer!');
+
+  cards.forEach((card) => {     // Adds EventListener for each cardDiv
+    card.addEventListener('click', (event) => {
+      event.target.classList.add('clicked'); // Flips card
+      clickArray.push(event.target.dataset.number); // Adds data number to array
+      matchArray.push(card);
+
+      if (clickArray.length === 2) { // Compare logic
+        if (clickArray[0] === clickArray[1]){ // If cards data number match
+          clickArray = [];
+          matchArray = [];
+          counter++;
+        } else {
+          setTimeout(()=> {
+          matchArray[0].classList.remove('clicked');
+          matchArray[1].classList.remove('clicked');
+          clickArray = [];
+          matchArray = [];
+          }, 800);
+        };
+      };
+
+      if (counter === pairs) {
+        document.querySelector('.complete').classList.add('visible');
+      };
+
+    });
+  });
+};
+// END GAME LOGIC
+
+
 /*
 * Shuffles an array
+*
+* @param {array} array
+* @returns {array} newArray
 */
 const shuffle = (array) => {
-  let newArray = [];
-  let n = array.length-1;
-  let i;
-  while (n >= 0) {
-    i = Math.floor(Math.random() * n--);
-    newArray.push(array.splice(i, 1)[0]);
-  };
+  const newArray = [];
+  array.forEach(item => {
+    const index = Math.floor(Math.random() * array.length | 0);
+    newArray.splice(index, 0, item);
+  });
   return newArray;
 };
 
+
 /*
-* Shuffles the children of board container
+* Resets board and counter
 */
 const resetBoard = () => {
-  // Removes class matched
-  const flipped = document.querySelectorAll('.matched');
-  flipped.forEach(card => {
-    card.classList.remove('matched');
-  });
-  // Removes class clicked
+  // Remove class clicked
   const clicked = document.querySelectorAll('.clicked');
   clicked.forEach(card => {
     card.classList.remove('clicked');
   });
-  // Shuffles position of divs
-  const board = document.querySelector('.board');
-  for (let i = board.children.length; i >= 0; i--) {
-     board.appendChild(board.children[Math.random() * i | 0]);
-   };
-   // Makes game over div visible
-   const complete = document.querySelector('.complete');
-   complete.classList.remove('visible');
-  // Resets the counter
+  // Clear board
+  document.querySelector('.board').innerHTML = '';
+  // Remove game over div
+  document.querySelector('.complete').classList.remove('visible');
+  // Reset the counter
   counter = 0;
 };
