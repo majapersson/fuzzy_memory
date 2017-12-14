8d57694ce49c5ac413f512b7c6beab5349b246f5
Maja Persson <persson.maja@gmail.com>
2017-12-14 11:45:41 +0100

    Beautification

--- js/functions.js
+++ js/functions.js
@@ -4,9 +4,9 @@
 * @param {array} array
 */
 const createBoard = (pairs) => {
-  for(let i=0; i<pairs; i++){   // Adds duplicates for each card in stack
+  for (let i = 0; i < pairs; i++) {   // Adds duplicates for each card in stack
     stack.push(i, i);
-  };
+  }
 
   const shuffledStack = shuffle(stack); // Shuffles stack
 
@@ -17,9 +17,9 @@ const createBoard = (pairs) => {
 
   document.querySelector('.start').classList.remove('visible');
   const resetButton = document.querySelector('body > button');
-  if (resetButton.classList.contains('invisible')){
+  if (resetButton.classList.contains('invisible')) {
     resetButton.classList.remove('invisible');
-  };
+  }
 };
 
 /*
@@ -38,19 +38,19 @@ const gameLogic = () => {
   cards.forEach((card) => {     // Adds EventListener for each cardDiv
     card.addEventListener('click', (event) => {
       event.target.classList.add('animation');
-      setTimeout(() => {event.target.classList.add('clicked');}, 200); // Flips card
+      setTimeout(() => { event.target.classList.add('clicked'); }, 200); // Flips card
       clickArray.push(event.target.dataset.number); // Adds data number to array
       matchArray.push(card);
       clickCounter++;
 
       if (clickArray.length === 2) { // Compare logic
         document.querySelector('body').classList.toggle('pointerNone');
-        if (clickArray[0] === clickArray[1]){ // If cards data number match
+        if (clickArray[0] === clickArray[1]) { // If cards data number match
           clickArray = [];
           matchArray = [];
           matchCounter++;
         } else {
-          setTimeout(()=> {
+          setTimeout(() => {
             matchArray[0].classList.remove('clicked');
             matchArray[0].classList.remove('animation');
             matchArray[1].classList.remove('clicked');
@@ -58,22 +58,20 @@ const gameLogic = () => {
             clickArray = [];
             matchArray = [];
           }, 800);
-        };
+        }
         setTimeout(() => {
           document.querySelector('body').classList.toggle('pointerNone');
         }, 800);
-      };
+      }
 
       if (matchCounter === pairs) {
-        document.querySelector('.complete h2').textContent = `You cleared the game with ${clickCounter/2} tries!`;
+        document.querySelector('.complete h2').textContent = `You cleared the game with ${clickCounter / 2} tries!`;
         document.querySelector('.complete').classList.add('visible');
-      };
-
+      }
     });
   });
 };
 // END GAME LOGIC
-
 
 /*
 * Shuffles an array
@@ -82,16 +80,15 @@ const gameLogic = () => {
 * @returns {array} newArray
 */
 const shuffle = (array) => {
-    let j, x, i;
-    for (i = array.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = array[i];
-        array[i] = array[j];
-        array[j] = x;
-    }
-    return array;
+  let j, x, i;
+  for (i = array.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = array[i];
+    array[i] = array[j];
+    array[j] = x;
+  }
+  return array;
 };
-
 
 /*
 * Resets board and matchCounter
